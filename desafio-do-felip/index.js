bd2cd7498e3f1c3347fe7bebf1d5f7f62c7b4a2d
xime <dielisonmoraes@gmail.com>
2024-01-11 16:46:35 -0300

    Pronto

--- desafio-do-felip/index.js
+++ desafio-do-felip/index.js
@@ -1,4 +1,31 @@
 let nome = "Zé Gotinha"
-let xp = 0
+let xp = 10000
+let elo = ""
 
-console.log("Olá, " + nome +" Seu XP é de: " + xp + "Seu elo é:" )
+switch (xp) {
+    case (xp < 1000):
+        elo = "Ferro";
+        break;
+    case (xp <= 2000):
+        elo = "Bronze";
+        break;
+    case (xp <= 5000):
+        elo = "Prata";
+        break;
+    case (xp <= 7000):
+        elo = "Ouro";
+        break;
+    case (xp <= 8000):
+        elo = "Platina";
+        break;
+    case (xp <= 9000):
+        elo = "Ascendente";
+        break;
+    case (xp <= 10000):
+        elo = "Imortal";
+        break;
+    default:
+        elo = "Radiante";
+        break;
+}
+console.log(`O herói de nome ${nome} está no nível de ${elo}.`);
